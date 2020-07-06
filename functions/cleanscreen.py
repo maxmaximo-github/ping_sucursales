3e54f54d0ef284d3aa5729f53dcb2a1005dd14b1
maxmaximo-github <max.maximo.solutions@gmail.com>
2020-07-06 14:17:08 -0500

    Update cleanscreen.py

--- functions/cleanscreen.py
+++ functions/cleanscreen.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env  python3
+# -*- coding: utf-8 -*-
+"""
+This script is reboot IPv4 phones.
+
+It seems that it has to have THIS docstring with a summary line, a blank line
+and sume more text like here. Wow.
+"""
 __author__ = "Cesar Rodriguez"
 __copyright__ = "Copyright 2020, Reboot IPv4 Phones"
 __credits__ = ["Cesar Rodriguez"]
@@ -14,6 +21,15 @@ from os import system
 
 
 def clean_screen():
+    """
+    Funcion para limpiar pantalla.
+
+    Funcion que limpia la pantalla de las salidas existentes para mostrar
+    solo los resultados que uno desee.
+
+    Se realiza una comprativa para determinar si se corre el comando para
+    GNU/Linux o para sistemas con Windows.
+    """
     if name == "nt":
         _ = system("cls")
     else:
