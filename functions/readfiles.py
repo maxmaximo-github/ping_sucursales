ce3437c610ce3265a8063eafac8814d344a2def4
maxmaximo-github <max.maximo.solutions@gmail.com>
2020-07-06 13:31:00 -0500

    Add function readfiles to project

---
+++ functions/readfiles.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env  python3
+# -*- coding: utf-8 -*-
+"""
+This script is reboot IPv4 phones.
+
+It seems that it has to have THIS docstring with a summary line, a blank line
+and sume more text like here. Wow.
+"""
+__author__ = "Cesar Rodriguez"
+__copyright__ = "Copyright 2020, Reboot IPv4 Phones"
+__credits__ = ["Cesar Rodriguez"]
+__license__ = "GPL"
+__version__ = "1.0.2"
+__maintainer__ = "Cesar Rodriguez"
+__email__ = "cesarrodriguez@gmail.com"
+__status__ = "Develop"
+
+
+import os
+import glob
+
+
+def read_files():
+    """
+    Funcion para leer los archivos de los telefonos que estan activos.
+
+    Esta funcion lee los archivos que se encuentran en la carpeta tmp de
+    los dispositivos que tuvieron exito a la hora de realizar la prueba de
+    ping.
+    """
+    directory = os.getcwd()
+    devices_alive = []
+    for file_name in glob.glob(f"{directory}/tmp/*"):
+        with open(file=file_name, mode="r") as f:
+            for line in f.readlines():
+                devices_alive.append(line)
+
+    return devices_alive
