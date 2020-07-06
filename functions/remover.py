c2bb1e804bda83bd9391fa3985a520e0081df8f5
maxmaximo-github <max.maximo.solutions@gmail.com>
2020-07-06 14:17:49 -0500

    Update remover.py

--- functions/remover.py
+++ functions/remover.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env  python3
+# -*- coding: utf-8 -*-
+"""
+This script is create for general propouse.
+
+It seems that it has to have THIS docstring with a summary line, a blank line
+and sume more text like here. Wow.
+"""
 __author__ = "Cesar Rodriguez"
 __copyright__ = "Copyright 2020, Reboot IPv4 Phones"
 __credits__ = ["Cesar Rodriguez"]
@@ -15,6 +22,14 @@ from os import remove
 
 
 def remover_files():
+    """
+    Funcion para eliminar archivos de la carpeta tmp.
+
+    Esta funcion elimina los archivos que se crean al realizar las acciones
+    por las diferentes fuciones que crean archivos y los guardan temporalmente
+    para su tratamiento posterior evitando asi que en ejecucion de hilos
+    posteriores estos sean leidos y realizado las acciones pertienentes.
+    """
     directory = os.getcwd()
     for file_name in glob.glob(("{}/tmp/*").format(directory)):
         remove(file_name)
